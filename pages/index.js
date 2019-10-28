5c23a26bd25e073d1e63c9fbf6da2aa047ba424f
crazy7th <ekkypatria@gmail.com>
2019-10-28 23:46:26 +0700

    [page] first page with nextjs

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => (
+    <div>
+        <h1>Ngoding itu Menyenangkan ☺</h1>
+
+        <p>Coba bayangin deh, dimulai dari baris demi baris kode yang nantinya kamu lakuin, kamu bisa bikin apapun yang kamu mau</p>
+
+        <button>Kuy Mabar</button>
+    </div>
+);
+  
+export default Index;
