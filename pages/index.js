0a7218e2362e6450c3fb93db6db0b70bd321f08a
crazy7th <ekkypatria@gmail.com>
2019-10-29 16:02:28 +0700

    [pwa] 2.add manifest

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Manifest from 'next-manifest/manifest'
+
 import { register, unregister } from 'next-offline/runtime'
 
 class Index extends React.Component {
@@ -16,6 +19,21 @@ class Index extends React.Component {
     render() {
         return (
             <div>
+                <Head>
+                    <title>My page title</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <meta name="theme-color" content="#000000" />
+                    <link rel="shortcut icon" href="/static/favicon.ico">
+                    <Manifest
+                        // path for manifest will be deploying
+                        href='/static/manifest.json'
+                        // color for `theme-color`
+                        //themeColor='#F0F0F0'
+                        // scale for `viewport` meta tag
+                        //initialScale='1'
+                    />
+                </Head>
+
                 <h1>Ngoding itu Menyenangkan ☺</h1>
 
                 <p>Coba bayangin deh, dimulai dari baris demi baris kode yang nantinya kamu lakuin, kamu bisa bikin apapun yang kamu mau</p>
