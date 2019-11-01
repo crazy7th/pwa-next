c50ecd2cd31c980423335d2db3f2570b82c1606f
satrioo <satrioari43@gmail.com>
2019-11-01 16:50:26 +0700

    link page

---
+++ learning/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+const homepage = () => (
+  <div>
+    home
+    <Link href="/index">asd </Link>
+  </div>
+);
+
+export default homepage;
---
+++ learning/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+const index = () => (
+  <div>
+    aasdasd
+    <Link href="/home">asd </Link>
+  </div>
+);
+
+export default index;
