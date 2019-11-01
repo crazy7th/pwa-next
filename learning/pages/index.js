e8db13e15af7c4f4400364bfcad703634c5aa66b
crazy7th <ekkypatria@gmail.com>
2019-11-01 18:42:58 +0700

    [save] PWA

--- learning/pages/index.js
+++ learning/pages/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
 
+// buat nambahin link rel, title, meta, dll
+import Head from 'next/head';
+
 const index = () => {
     return (
         <div>
+            <Head>
+                <title>My Cool App</title>
+                <link rel="manifest" href="/static/manifest.json" />
+            </Head>
             <h1>Hello World</h1>
             <p>Ini splash screen pertama website kita</p>
 
